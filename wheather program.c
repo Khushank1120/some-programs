098eb81f6903c8ecbbe7f457e5e80aeca40dc048
preetmodh <54811605+preetmodh@users.noreply.github.com>
2019-12-31 18:47:27 +0530

    Update wheather program.c
    A weather program that calculates average yearly and monthly rainfall in 3 years using arrays.

--- wheather program.c
+++ wheather program.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 
+    // TYPES 12 VALUES PER MONTH IN A YEAR AND IT WILL TELL YOU YEARLY AND MONTHLY AVG RAINFALL.
 
 int main()
 {
 
-    float rainfall[5][12];
+    float rainfall[3][12];
 
     float total;
     float subtotal;
     int i ;
     int j;
 
-        for(i=0;i<5;i++)
+        for(i=0;i<3;i++)
         {
                 printf("enter 12 values of %d  year per month:",i+1 );
                 for(j=0;j<12;j++)
@@ -22,7 +23,7 @@ int main()
 
      printf("YEAR\t\tRAINFALL  (inches)\n");
 
-                for(i=0,total=0;i<5;i++)
+                for(i=0,total=0;i<3;i++)
                 {
                     for(j=0,subtotal=0;j<12;j++)
                     {
@@ -31,18 +32,18 @@ int main()
                          printf("%5d \t%15.1f\n" ,2015 + i , subtotal);
                          total+=subtotal;
                 }
-                printf("\nThe yearly average is %.1f inches.\n\n",total/5);
+                printf("\nThe yearly average is %.1f inches.\n\n",total/3);
 
                 printf("MONTHLY AVERAGES:\n\n");
                 printf(" Jan   Feb   Mar   Apr   May   Jun   Jul   Aug   Sep   Oct   Nov   Dec\n");
 
                 for(i=0;i<12;i++)
                 {
-                    for(j=0,subtotal=0;j<5;j++)
+                    for(j=0,subtotal=0;j<3;j++)
                     {
                           subtotal += rainfall[j][i];
                     }
-                          printf("%5.1f ", subtotal/5);
+                          printf("%5.1f ", subtotal/3);
                 }
 
                 return 0;
